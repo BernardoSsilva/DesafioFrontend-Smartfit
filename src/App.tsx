01b5a61bfe8ec33344b4c422e6c5bb982f0001b4
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2023-12-28 14:46:56 -0300

    responsive layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Header from './components/Header';
-import SearchBox from './components/SearchBox';
-import Legend from './components/Legend';
-import ResultCard from './components/ResultCard';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./components/Header";
+import SearchBox from "./components/SearchBox";
+import Legend from "./components/Legend";
+import ResultCard from "./components/ResultCard";
 
 function App() {
   const [value, setValue] = useState(0);
@@ -12,45 +12,44 @@ function App() {
   const [results, setResults] = useState<any[]>([]);
   const cards: any[] = [];
 
-
   function search() {
-    
-    axios.get("https://test-frontend-developer.s3.amazonaws.com/data/locations.json")
+    axios
+      .get(
+        "https://test-frontend-developer.s3.amazonaws.com/data/locations.json"
+      )
       .then((response) => {
         const data = response.data;
         const total = response.data.total;
         for (let i = 0; i < total; i++) {
           cards.push(data.locations[i]);
-
         }
         console.log(cards);
         setResults(cards);
         setValue(total);
       })
       .catch((error) => {
-        console.error('Ocorreu um erro ao buscar os dados:', error);
+        console.error("Ocorreu um erro ao buscar os dados:", error);
       });
   }
 
-  function clear(event:any) {
+  function clear(event: any) {
     window.location.reload();
   }
 
   return (
     <>
-      <Header />
-      <SearchBox results={value} search={search} function2={clear} />
-      <Legend />
-      <div className='resultRow' >
-        {value > 0 ? (
-          results.map((item, index) => (
-            <ResultCard item={item} />
-          ))
-        ) : (
-          <p>Nenhum resultado encontrado.</p>
-        )}
+      <div className="mainBody">
+        <Header />
+        <SearchBox results={value} search={search} function2={clear} />
+        <Legend />
+        <div className="resultRow">
+          {value > 0 ? (
+            results.map((item, index) => <ResultCard item={item} />)
+          ) : (
+            <p>Nenhum resultado encontrado.</p>
+          )}
+        </div>
       </div>
-
     </>
   );
 }
